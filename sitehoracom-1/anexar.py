7468bbcd8c4d17adf6b8dbbef386f37c111a12c8
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-10-15 20:52:45 -0300

    Update anexar.py

--- sitehoracom-1/anexar.py
+++ sitehoracom-1/anexar.py
@@ -1,20 +1,26 @@
-import sqlite3
+import mysql.connector
 
-# Função para inserir um certificado na tabela Certificados
+# Função para inserir um certificado na tabela Certificados do MySQL
 def inserir_certificado(id_aluno, grupo, opcao, quantidade_horas):
-    # Conectar ao banco de dados SQLite
-    conn = sqlite3.connect('horacom.db')
-    cursor = conn.cursor()
-
     try:
+        # Conectar ao banco de dados MySQL
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='amarelo123*',
+            database='horacom'
+        )
+        cursor = conn.cursor()
+
         # Inserir os dados na tabela Certificados
-        cursor.execute("INSERT INTO Certificados (id_aluno, grupo, opcao, quantidade_horas) VALUES (?, ?, ?, ?)",
-                       (id_aluno, grupo, opcao, quantidade_horas))
+        query = "INSERT INTO Certificados (id_aluno, grupo, opcao, quantidade_horas) VALUES (%s, %s, %s, %s)"
+        values = (id_aluno, grupo, opcao, quantidade_horas)
+        cursor.execute(query, values)
 
         # Commit para salvar as alterações
         conn.commit()
         print("Certificado inserido com sucesso!")
-    except sqlite3.Error as e:
+    except mysql.connector.Error as e:
         print("Erro ao inserir certificado:", str(e))
     finally:
         # Fechar a conexão com o banco de dados
@@ -22,4 +28,5 @@ def inserir_certificado(id_aluno, grupo, opcao, quantidade_horas):
 
 # Exemplo de uso da função
 inserir_certificado(1, 'g1', 'Opção 1', 30.5)
+
 
