4a2b25b3503ada0f6a0d2086abebd3496cdddd5f
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-10-29 18:16:56 -0300

    Create config.py

---
+++ sitehoracom/routes/config.py
@@ -0,0 +1,11 @@
+
+def get_db_config():
+    # Aqui você deve retornar os detalhes de configuração do banco de dados, como um dicionário
+    db_config = {
+        'user': 'root',
+        'password': 'amarelo123*',
+        'host': 'localhost',
+        'database': 'HoraCom'
+        # outros detalhes de configuração, se aplicável
+    }
+    return db_config
