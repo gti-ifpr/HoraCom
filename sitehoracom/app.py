45f6194aa8119cf5cb0f98391e167e7da492bfad
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-10-29 17:18:41 -0300

    Create app.py

---
+++ sitehoracom/app.py
@@ -0,0 +1,97 @@
+import mysql.connector
+from routes.cadastro import cadastro_bp
+from routes.login import login_bp
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__, static_folder='static', static_url_path='') 
+app.register_blueprint(cadastro_bp)
+app.register_blueprint(login_bp)
+
+@app.route('/')
+def index():
+    return render_template('index/index.html')
+
+@app.route('/acesso')
+def login():
+    return render_template('index/login/login.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('index/contato.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('index/sobre.html')
+
+@app.route('/sobre/storyboard')
+def storyboard():
+    return render_template('index/sobre/storyboard.html')
+
+@app.route('/sobre/idealizadoras')
+def idealizadoras():
+    return render_template('index/sobre/idealizadoras.html')
+
+@app.route('/sobre/persona')
+def persona():
+    return render_template('index/sobre/persona.html')
+
+@app.route('/processar_cadastro', methods=['POST'])
+    # Lógica para processar o cadastro
+def processar_cadastro():
+    return 'Cadastro processado com sucesso!'
+
+@app.route('/esqueceusenha')
+def esqueceu_senha():
+    # Lógica para página de esqueceu a senha
+    return render_template('index/login/esqueceusenha.html')
+
+@app.route('/usercoordenador')
+def user_coordenador():
+    # Lógica para a página do coordenador
+    return render_template('index/login/usercoordenador.html')
+
+@app.route('/login/cadastro')
+def cadastro():
+    # Lógica para página de cadastro
+    return render_template('index/login/cadastro.html')
+
+@app.route('/login/editarcadastro')
+def editar_cadastro():
+    # Lógica para editar o cadastro
+    return render_template('index/login/editarcadastro.html')
+
+@app.route('/relatorio')
+def relatorio():
+    # Lógica para a página de relatório
+    return render_template('relatorio.html')
+
+@app.route('/saibamais')
+def saibamais():
+    # Lógica para a página de saibamais
+    return render_template('saibamais.html')
+
+@app.route('/upload')
+def upload():
+    # Lógica para a página de upload
+    return render_template('upload.html')
+
+@app.route('/editar')
+def editar():
+    # Lógica para a página de editar
+    return render_template('editar.html')
+
+@app.route('/tabela')
+def tabela():
+    # Lógica para a página de editar
+    return render_template('tabela.html')
+
+@app.route('/extrairzip')
+def extrair():
+    # Lógica para a página de editar
+    return render_template('extrairzip.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
