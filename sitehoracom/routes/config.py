7c7195e05ad770597979b5a1cef0f8f4a6ed4a63
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-11-08 23:24:41 -0300

    Update config.py

--- sitehoracom/routes/config.py
+++ sitehoracom/routes/config.py
@@ -1,3 +1,4 @@
+import mysql.connector
 
 def get_db_config():
     # Aqui você deve retornar os detalhes de configuração do banco de dados, como um dicionário
@@ -5,7 +6,24 @@ def get_db_config():
         'user': 'root',
         'password': 'amarelo123*',
         'host': 'localhost',
-        'database': 'HoraCom'
+        'database': 'horacom'
         # outros detalhes de configuração, se aplicável
     }
     return db_config
+
+
+
+def conectar_banco_de_dados():
+    try:
+        db_config = get_db_config()  # Obtendo os detalhes de configuração do banco de dados
+        conexao = mysql.connector.connect(**db_config)  # Conectando ao banco de dados
+        
+        if conexao.is_connected():
+            print("Conexão bem-sucedida ao banco de dados!")
+            conexao.close()  # Fechando a conexão
+    except mysql.connector.Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+
+# Chama a função para testar a conexão com o banco de dados
+conectar_banco_de_dados()
+
