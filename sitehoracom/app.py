958d8847c4b513fbbaffa026de0994a74302dd5e
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-11-10 00:02:33 -0300

    Update app.py

--- sitehoracom/app.py
+++ sitehoracom/app.py
@@ -3,7 +3,7 @@ from routes.login import login_bp
 from flask import Flask, render_template, request, redirect, url_for
 from routes.config import get_db_config
 import mysql.connector
-
+from routes.cadastro import processar_cadastro
 
 app = Flask(__name__, static_folder='static', static_url_path='') 
 app.register_blueprint(cadastro_bp)
@@ -18,9 +18,52 @@ def initialize_database():
 def index():
     return render_template('index/index.html')
 
+from flask import request, render_template, redirect, url_for, session
+
 @app.route('/acesso')
 def login():
+    if 'email' in session:
+        # Usuário já está logado
+        return redirect(url_for('pagina_de_redirecionamento'))
     return render_template('index/login/login.html')
+
+@app.route('/processar_login', methods=['POST'])
+def processar_login():
+    email = request.form['email']
+    senha = request.form['senha']
+
+    conexao = mysql.connector.connect(**get_db_config())
+    cursor = conexao.cursor()
+
+    consulta = f"SELECT tipo_usuario FROM usuarios WHERE email = '{email}' AND senha = '{senha}'"
+
+    cursor.execute(consulta)
+    resultado = cursor.fetchone()
+
+    cursor.close()
+    conexao.close()
+
+    if resultado:
+        tipo_usuario = resultado[0]
+        session['email'] = email  # Defina o email na sessão
+        if tipo_usuario == 'academico':
+            return redirect(url_for('pagina_do_academico'))
+        elif tipo_usuario == 'coordenador':
+            return redirect(url_for('pagina_do_coordenador'))
+        else:
+            return redirect(url_for('acesso'))
+
+    return redirect(url_for('acesso')) 
+
+# Páginas para usuários de diferentes tipos
+@app.route('/pagina_do_academico')
+def pagina_do_academico():
+    return render_template('index/usuario/useracademic.html')
+
+@app.route('/pagina_do_coordenador')
+def pagina_do_coordenador():
+    return render_template('index/usuario/usercoordenador.html')
+
 
 @app.route('/contato')
 def contato():
@@ -43,9 +86,30 @@ def persona():
     return render_template('index/sobre/persona.html')
 
 @app.route('/processar_cadastro', methods=['POST'])
-    # Lógica para processar o cadastro
 def processar_cadastro():
-    return 'Cadastro processado com sucesso!'
+    
+    email = request.form['email']
+    senha = request.form['senha']
+
+    # Exemplo simplificado de conexão com o banco e inserção de dados
+    conexao = mysql.connector.connect(**get_db_config())
+    cursor = conexao.cursor()
+
+    # Execute a lógica de inserção no banco de dados com os dados fornecidos
+    try:
+        consulta = f"INSERT INTO tabela_usuarios ( email, senha) VALUES ( '{email}', '{senha}')"
+        cursor.execute(consulta)
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+        return 'Cadastro processado com sucesso!'
+    except Exception as e:
+        # Em caso de erro, desfaça a operação e retorne uma mensagem de erro
+        conexao.rollback()
+        cursor.close()
+        conexao.close()
+        return 'Erro no processamento do cadastro.'
+
 
 @app.route('/esqueceusenha')
 def esqueceu_senha():
@@ -101,9 +165,6 @@ def tabela():
 def extrair():
     # Lógica para a página de editar
     return render_template('extrairzip.html')
-
-
-
 
 
 
