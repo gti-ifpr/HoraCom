15d63fa695b3e989807e48a29d12955a77c291ba
Fernanda Teixeira <123215346+fernandat87@users.noreply.github.com>
2023-11-12 23:50:29 -0300

    Update login.py

--- sitehoracom/routes/login.py
+++ sitehoracom/routes/login.py
@@ -1,50 +1,31 @@
-from flask import request, redirect, url_for, render_template, Blueprint, current_app, session
-import mysql.connector
-import config
-
+from flask import request, redirect, url_for, render_template, Blueprint, current_app, session, flash
+from flask_login import login_user, logout_user
+from werkzeug.security import check_password_hash
+from .models import User  # Suponhamos que você tenha uma classe de modelo User
 
 login_bp = Blueprint("login", __name__)
 
-# Rota para exibir o formulário de login
 @login_bp.route('/login', methods=['GET'])
 def login():
     return current_app.send_static_file('login.html')
 
-# Rota para processar o envio do formulário de login
 @login_bp.route('/processar_login', methods=['POST'])
 def processar_login():
     email = request.form['email']
-    senha = request.form['senha']
+    senha_digitada = request.form['senha']
 
-    conexao = mysql.connector.connect(**config.get_db_config())
+    user = User.query.filter_by(email=email).first()
 
-    try:
-        cursor = conexao.cursor()
-        consulta = f"SELECT * FROM horacom.aluno WHERE email = '{email}' AND senha = '{senha}'"
-        cursor.execute(consulta)
-        #print(consulta)
-        resultado = cursor.fetchone()
+    if user and check_password_hash(user.senha_hash, senha_digitada):
+        login_user(user)
+        flash('Login bem-sucedido!', 'success')
+        return redirect(url_for('perfil'))  # Substitua 'perfil' pelo nome da sua rota de perfil
+    else:
+        flash('Credenciais inválidas. Tente novamente.', 'danger')
+        return redirect(url_for('.login'))
 
-        if resultado:
-            # Defina uma variável de sessão para rastrear o usuário logado
-            session['user_id'] = resultado[0]
-            # Redirecione para a página de perfil do usuário ou outra página após o login
-            return redirect(url_for('perfil'))  # Substitua 'perfil' pelo nome da sua rota de perfil
-    
-    except Exception as e:
-        # print(e)
-        conexao.rollback()
-
-
-    finally:
-        cursor.close()
-        conexao.close()
-
-    # Redireciona de volta para a página de login em caso de falha de autenticação
-    return redirect(url_for('.login'))
-
-# Rota de logout
 @login_bp.route('/logout')
 def logout():
-    session.pop('user_id', None)  # Remove a variável de sessão para encerrar a sessão do usuário
+    logout_user()
+    flash('Logout bem-sucedido!', 'success')
     return redirect(url_for('.login'))
